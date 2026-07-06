cda0d4c8b5b468358f926a5672824e18f6ce6b41
DAVID <owolagbadavid@gmail.com>
2026-07-06 09:07:14 +0100

    Add: binary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-0.rs
@@ -0,0 +1,38 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//     pub val: i32,
+//     pub left: Option<Rc<RefCell<TreeNode>>>,
+//     pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//     #[inline]
+//     pub fn new(val: i32) -> Self {
+//         TreeNode {
+//             val,
+//             left: None,
+//             right: None,
+//         }
+//     }
+// }
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut res = Vec::new();
+        Self::post(&root, &mut res);
+        res
+    }
+
+    fn post(root: &Option<Rc<RefCell<TreeNode>>>, res: &mut Vec<i32>) {
+        if let Some(root) = root {
+            let root = root.borrow();
+            Self::post(&root.left, res);
+            Self::post(&root.right, res);
+            res.push(root.val);
+        }
+    }
+}
