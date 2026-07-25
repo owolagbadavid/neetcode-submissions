967c65dc15150d8d31194c883532718c8536b362
DAVID <owolagbadavid@gmail.com>
2026-07-25 15:31:21 +0100

    Add: island-perimeter - submission-2

---
+++ Data Structures & Algorithms/island-perimeter/submission-2.rs
@@ -0,0 +1,41 @@
+impl Solution {
+    pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
+        let mut res = 0;
+        let directions = vec![(0, 1), (0, -1), (1, 0), (-1, 0)];
+
+        let rows = grid.len();
+        let cols = grid[0].len();
+        let mut start = (0, 0);
+
+        'outer: for i in 0..rows {
+            for j in 0..cols {
+                if grid[i][j] == 1 {
+                    start = (i, j);
+                    break 'outer;
+                }
+            }
+        }
+
+        let mut q = VecDeque::from([start]);
+        let mut visit = vec![vec![false; cols]; rows];
+        visit[start.0][start.1] = true;
+
+        while let Some((r, c)) = q.pop_front() {
+            for (dr, dc) in &directions {
+                let (nr, nc) = (r as i32 + dr, c as i32 + dc);
+                if nr < 0 || nc < 0 || nr >= rows as i32 || nc >= cols as i32 || grid[nr as usize][nc as usize] == 0 {
+                    res += 1;
+                    continue;
+                }
+
+                let (nr, nc) = (nr as usize, nc as usize);
+                if !visit[nr][nc] {
+                    q.push_back((nr, nc));
+                    visit[nr][nc] = true;
+                }
+            }
+        }
+
+        res
+    }
+}
