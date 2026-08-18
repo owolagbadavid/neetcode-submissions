8501f47da29d08e01b9d59998f9fcf1fd9b34fac
DAVID <owolagbadavid@gmail.com>
2026-08-18 19:47:15 +0100

    Add: longest-turbulent-subarray - submission-2

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-2.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn max_turbulence_size(arr: Vec<i32>) -> i32 {
+        let mut res = 1;
+        let mut last: Option<bool> = None;
+        let mut l = 0;
+        for r in 1..arr.len() {
+            let next = if arr[r-1] > arr[r] { Some(true) } else if arr[r-1] < arr[r] { Some(false) } else { None };
+            if next.is_none() || last == next {
+                l = if next.is_none() {r} else {r-1};
+                last = next;
+            }
+            last = next;
+            
+            res = res.max((r-l+1) as i32);
+        }
+
+        res
+    }
+}
