59d534f959a77cbcb64e79dff8c578af5cccac77
DAVID <owolagbadavid@gmail.com>
2026-08-19 11:38:40 +0100

    Add: jump-game-vii - submission-1

---
+++ Data Structures & Algorithms/jump-game-vii/submission-1.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    pub fn can_reach(s: String, min_jump: i32, max_jump: i32) -> bool {
+
+        fn dfs(i: usize, chars: &[char], min_jump: usize, max_jump: usize, memo: &mut [Option<bool>]) -> bool {
+            if i == chars.len() - 1 {
+                return true;
+            }
+
+            if let Some(res) = memo[i] {
+                return res;
+            }
+
+            let mut res = false;
+            for j in min_jump..=max_jump {
+                if i+j < chars.len() && chars[i+j] == '0' {
+                    res = res || dfs(i+j, chars, min_jump, max_jump, memo);
+                }
+            }
+
+            memo[i] = Some(res);
+
+            res
+        }
+        dfs(0, &s.chars().collect::<Vec<char>>(), min_jump as usize, max_jump as usize, &mut vec![None; s.len()])
+    }
+}
