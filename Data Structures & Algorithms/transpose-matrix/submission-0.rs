6d5379aaa8feee8ce1e31eb7316a9a66937071a1
DAVID <owolagbadavid@gmail.com>
2026-08-26 10:31:45 +0100

    Add: transpose-matrix - submission-0

---
+++ Data Structures & Algorithms/transpose-matrix/submission-0.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn transpose(matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut res = vec![vec![0; matrix.len()]; matrix[0].len()];
+
+        for i in 0..matrix.len() {
+            for j in 0..matrix[0].len() {
+                res[j][i] = matrix[i][j];
+            }
+        }
+
+        res
+    }
+}
