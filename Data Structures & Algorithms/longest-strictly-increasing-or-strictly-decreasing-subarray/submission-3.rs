9c39c8843c4aad9d227aeb91d7cc5898d4d7c678
DAVID <owolagbadavid@gmail.com>
2026-09-14 09:27:10 +0100

    Add: longest-strictly-increasing-or-strictly-decreasing-subarray - submission-3

---
+++ Data Structures & Algorithms/longest-strictly-increasing-or-strictly-decreasing-subarray/submission-3.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn longest_monotonic_subarray(nums: Vec<i32>) -> i32 {
+        let mut last: Option<i32> = None;
+        let mut res = 0;
+        let mut count = 0;
+
+        for i in 0..nums.len() {
+            if last.is_some() && last.unwrap() >= nums[i] {
+                count = 0;
+            }
+            count += 1;
+            last = Some(nums[i]);
+            res = res.max(count);
+        }
+
+        count = 0;
+        last = None;
+
+        for i in 0..nums.len() {
+            if  last.is_some() && last.unwrap() <= nums[i] {
+                count = 0;
+            }
+            count += 1;
+            last = Some(nums[i]);
+            res = res.max(count);
+        }
+
+        res
+    }
+}
